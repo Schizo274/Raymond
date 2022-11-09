90a4a9eedc7d5fba6218a18728304a91d12a7f66
idonthvgithub <117578433+idonthvgithub@users.noreply.github.com>
2022-11-09 18:08:03 +0800

    Update Average cgpa.py

--- Average cgpa.py
+++ Average cgpa.py
@@ -1,33 +1,43 @@
-#1： Write File 
-PList = {
-    "1": "Foundation in Engineering",
-    "2": "Foundation in Arts",
-    "3": "Foundation in Computing",
-    "4": "Foundation in Business",
-    "5": "Foundation in Science"}
+PList = {"1": "Foundation in Engineering",
+         "2": "Foundation in Arts",
+         "3": "Foundation in Computing",
+         "4": "Foundation in Business",
+         "5": "Foundation in Science"}
 
-def WriteFile(name,id,prog,marks,*file):
-    if file != None:
-        f = open("StudentData.txt","a")
-        f.write(f"Name:{name}\tID:{id}\tProgramme:{prog}\tMarks:{marks}\n")
-    else:
-        file.a(f"Name:{name}\tID:{id}\tProgramme:{prog}\tMarks:{marks}\n")
+def WriteFile(name, id, prog, marks, *file):
+    try:
+        f = open(file, "a")
+        f.write(f"\n{id}\t{prog}\t{name}\t{marks}")
+    except:
+        with open("StudentData.txt", "a") as out:
+            out.write(f"\n{id}\t{prog}\t{name}\t{marks}")
+
+def Search():   
+    word = input("Enter a word to search >> ")
+    word = word.lower()
+    print("ID\tProgramme\tName\tMarks")
+    with open("StudentData.txt") as file:
+        lines = file.readlines()
+    count = 0
+    for i in range(len(lines)):
+        if word in lines[i].lower():
+            print(lines[i])
+            count += 1
+    print(count, "matches.")
+
 
 StudentName = input("Enter your name: ")
 ID = input("Enter your student id: ")
-Program = input(f"Enter your program: {PList}")
-SubjectNumber = input("Enter number of subjects：")
+Program = input(f"Enter your program: {PList}: ")
+SubjectNumber = input("Enter number of subjects: ")
 marks = []
-
 for i in range(0,eval(SubjectNumber)):
     CurrentMarks = input("Enter marks: ")
     marks.append(CurrentMarks)
 
 WriteFile(StudentName,ID,PList[Program],marks)
+Search()
 
-
-
-#2: Calculation
 def  calculate_marks(gpa_list):
     total=0
     
@@ -42,8 +52,12 @@ def calculate():
         lines=file.read().splitlines()
 
         for i in lines:
-            split_lines=i.split("\t")
-            GPAS=split_lines[3:][0][8:-2].split("', '")
-            print(f" Marks: {calculate_marks(GPAS)} .")
+            if i!="":
+                split_lines=i.split("\t")
+                ID=split_lines[0]
+                Program=split_lines[1]
+                name=split_lines[2]                
+                GPAS=split_lines[3:][0][2:-2].split("', '")
+                print(f" {name}\t{ID}\t{Program}\tMarks: {calculate_marks(GPAS)} .")
 
 calculate()
