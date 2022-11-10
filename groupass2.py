223417ccf35191d175024f578c9959b436263894
4skindoctor <117586349+4skindoctor@users.noreply.github.com>
2022-11-10 20:54:13 +0800

    Update groupass2.py

--- groupass2.py
+++ groupass2.py
@@ -4,23 +4,25 @@ PList = {"1": "Foundation in Engineering",
          "4": "Foundation in Business",
          "5": "Foundation in Science"}
 
-# def WriteFile(name, id, prog, marks):
-#     print(f"\n{id}\t{prog}\t{name}\t{marks}")
-    # with open("StudentData.txt", "a") as out:
-    #     out.write(f"\n{id}\t{prog}\t{name}\t{marks}")
-
 def ReadFile(FileName):
+    count = 0
     with open(FileName, "r") as file:
         Lines = file.readlines()
+        print("Student ID\tProgramme\tName\tGPA")
+        with open("GPAs.txt", "a") as out:
+            out.write("Student ID\tProgramme\tName\tGPA")
         for l in Lines:
             SplitLines = l.split(",")
-            return Calculate([int(mark) for mark in SplitLines[3:]])
+            print(f"{SplitLines[0]}\t{SplitLines[1]}\t{SplitLines[2]}\t{Calculate([int(mark) for mark in SplitLines[3:]])}")
+            with open("GPAs.txt", "a") as out:
+                out.write(f"\n{SplitLines[0]}\t{SplitLines[1]}\t{SplitLines[2]}\t{Calculate([int(mark) for mark in SplitLines[3:]])}")
+    print("This data has also been written to a file named GPAs.txt in the same folder as this programme.")
 
-def Calculate(MarkList): #this needs to be changed to suit the new syntax 
+def Calculate(MarkList):
     total = 0
     count = 0 
     for Marks in MarkList:
-        if Marks >= 80:
+        if Marks >= 80 and Marks <= 100:
             gpa = 4.0
         elif Marks >= 75 and Marks < 80:
             gpa = 3.67
@@ -68,11 +70,11 @@ def ManualInput(name, id, prog, SubjectNumber): #this fucntion no longer works
         CurrentMarks = int(input("Enter marks >> "))
         marks.append(int(CurrentMarks))
         gpa = Calculate(marks)
-        for mark in marks:
-            print(mark)
-    print(f"\n{id}\t{PList[prog]}\t{name}\t{marks}\t{gpa}")
+    print(f"{id}\t{PList[prog]}\t{name}\t{marks}\t{gpa}")
     with open("StudentData.txt", "a") as out:
         out.write(f"\n{id}\t{PList[prog]}\t{name}\t{marks}\t{gpa}")
+
+count = 0
 
 print("1 -> Enter data in a text file with format ID Programme Name Marks")
 print("2 -> Enter data directly into the programme")
@@ -81,38 +83,35 @@ print("4 -> Search for a specific entry in the data")
 print("5 -> Export data to a text file")
 print("6 -> End programme")
 
-# while True:
-#     try:
-#         option = int(input("Enter your option >> "))
-#         if option == 1:
-#             FileName = input("Please enter the file name and ensure it is placed in the same folder as this programme >> ")
-#             ReadFile(FileName)
-#         elif option == 2:
-#             StudentName = input("Enter the name of student >> ")
-#             ID = input("Enter the student id >> ")
-#             Program = input(f"Enter program: {PList} >> ")
-#             SubjectNumber = input("Enter number of subjects >> ")
-#             ManualInput(StudentName, ID, Program, SubjectNumber)
-#         elif option == 3:
-#             #activate cgpa calc
-#             ReadFile("StudentData.txt")
-#         elif option == 4:
-#             #search function
-#             Search()
-#         elif option == 5:
-#             #export f(x)
-#             pass
-#         elif option == 6:
-#             break
-#         else:
-#             print("Please enter a valid option!")
-#     except:
-#         print("Please enter a valid option!")
-
-
-FileName = input("Please enter the file name and ensure it is placed in the same folder as this programme >> ")
-print(ReadFile(FileName))
-
-
-# WriteFile(StudentName,ID,PList[Program],marks)
-# Search()
+while True:
+    try:
+        option = int(input("Enter your option >> "))
+        if option == 1:
+            FileName = input("Please enter the file name and ensure it is placed in the same folder as this programme >> ")
+            print(ReadFile(FileName))
+        elif option == 2:
+            StudentName = input("Enter the name of student >> ")
+            ID = input("Enter the student id >> ")
+            Program = input(f"Enter program: {PList} >> ")
+            SubjectNumber = input("Enter number of subjects >> ")
+            if count == 0:
+                with open("StudentData.txt", "a") as out:
+                    out.write("Student ID\tProgramme\tName\tMarks\tGPA")
+                count += 1
+            ManualInput(StudentName, ID, Program, SubjectNumber)
+        elif option == 3:
+            #activate cgpa calc
+            ReadFile("StudentData.txt")
+        elif option == 4:
+            #search function
+            Search()
+        elif option == 5:
+            #export f(x)
+            pass
+        elif option == 6:
+            print("Closing programme...")
+            break
+        else:
+            print("Please enter a valid option!")
+    except:
+        print("Please enter a valid option!")
